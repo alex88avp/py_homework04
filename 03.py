79e509d91549e9e66dd5a7cdda68bd4f650f1af7
aleksander <petrov_records@mail.ru>
2022-12-26 09:50:35 +0400

    список неповторяющихся элементов

--- 03.py
+++ 03.py
@@ -1,1 +1,7 @@
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+original_list = list(map(int, input("Введите числа:").split()))
+print(original_list)
+new_list = []
+[new_list.append(i) for i in original_list if i not in new_list]
+print(new_list)
